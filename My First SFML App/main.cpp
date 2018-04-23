d338cb1ee22ca3c778abe025fb96a66220f11f85
f3e7n2g1@gmail.com <f3e7n2g1@gmail.com>
2018-04-22 23:44:36 -0700

    setup up/down/left/right moves with corresponding icon

--- My First SFML App/main.cpp
+++ My First SFML App/main.cpp
@@ -20,72 +20,149 @@
 // Here is a small helper for you! Have a look.
 #include "ResourcePath.hpp"
 
+
+// Create the main window
+sf::RenderWindow* win; // = new window(sf::VideoMode(1280, 640), "Jessie's Escape");
+sf::Music *music;
+
+sf::Texture txtBackground;
+sf::Texture txtChar;
+
+sf::Sprite spriteBackground;
+sf::Sprite spriteCharacter;
+sf::Sprite* spriteCharDisp;
+sf::Sprite spriteCharl, spriteCharr, spriteCharu, spriteChard;
+
+namespace
+{
+    int initlize()
+    {
+        win = new sf::RenderWindow(sf::VideoMode(1600, 900), "Jessie's Escape");
+        music = new sf::Music;
+        
+        // Set the Icon
+        sf::Image icon;
+        if (!icon.loadFromFile(resourcePath() + "icon.png")) {
+            return EXIT_FAILURE;
+        }
+        win->setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+        
+        // Load a music to play        
+        if (!music->openFromFile(resourcePath() + "music.ogg")) {
+            return EXIT_FAILURE;
+        }
+        
+        // load textures
+        if (!txtBackground.loadFromFile(resourcePath() + "background.png")){
+            return EXIT_FAILURE;
+        }
+        
+        if (!txtChar.loadFromFile(resourcePath() + "character.png")){
+            return EXIT_FAILURE;
+        }
+        
+        spriteBackground.setTexture(txtBackground);
+        spriteCharacter.setTexture(txtChar);
+        
+        spriteCharr = spriteCharacter; spriteCharr.setTextureRect(sf::IntRect(50, 20, 90, 180));
+        spriteChard = spriteCharacter; spriteChard.setTextureRect(sf::IntRect(150, 20, 120, 180));
+        spriteCharl = spriteCharacter; spriteCharl.setTextureRect(sf::IntRect(280, 20, 90, 180));
+        spriteCharu = spriteCharacter; spriteCharu.setTextureRect(sf::IntRect(370, 20, 120, 180));
+        
+        const float scaleRatioChar = 0.5f;
+        spriteCharr.setScale(scaleRatioChar, scaleRatioChar);
+        spriteChard.setScale(scaleRatioChar, scaleRatioChar);
+        spriteCharl.setScale(scaleRatioChar, scaleRatioChar);
+        spriteCharu.setScale(scaleRatioChar, scaleRatioChar);
+        
+        spriteCharDisp = &spriteChard;
+        
+        return EXIT_SUCCESS;
+    }
+}
+
+void shutdown()
+{
+    delete music;
+    delete win;
+}
+
 int main(int, char const**)
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
+    if (initlize() == EXIT_FAILURE){
+        shutdown();
         return EXIT_FAILURE;
     }
 
     // Play the music
-    music.play();
-
+    music->play();
+    
     // Start the game loop
-    while (window.isOpen())
+    while (win->isOpen())
     {
         // Process events
         sf::Event event;
-        while (window.pollEvent(event))
+        while (win->pollEvent(event))
         {
             // Close window: exit
             if (event.type == sf::Event::Closed) {
-                window.close();
+                win->close();
+                break;
             }
-
+            
+            
+            int deltaMove = 20;
             // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Escape)
+                    {win->close(); continue;}
+                
+                if (event.key.code == sf::Keyboard::Left)
+                {
+                    spriteCharl.setPosition(spriteCharDisp->getPosition());
+                    spriteCharDisp = &spriteCharl;
+                    spriteCharDisp->move(-deltaMove,0);
+                }
+                
+                if (event.key.code == sf::Keyboard::Right)
+                {
+                    spriteCharr.setPosition(spriteCharDisp->getPosition());
+                    spriteCharDisp = &spriteCharr;
+                    spriteCharDisp->move(deltaMove,0);
+                    
+                }
+                
+                if (event.key.code == sf::Keyboard::Up)
+                {
+                    spriteCharu.setPosition(spriteCharDisp->getPosition());
+                    spriteCharDisp = &spriteCharu;
+                    spriteCharDisp->move(0, -deltaMove);
+                }
+                
+                if (event.key.code == sf::Keyboard::Down)
+                {
+                    spriteChard.setPosition(spriteCharDisp->getPosition());
+                    spriteCharDisp = &spriteChard;
+                    spriteCharDisp->move(0, deltaMove);
+                }
             }
         }
-
+        
+        if (!win->isOpen()) continue;
+        
         // Clear screen
-        window.clear();
+        win->clear();
 
         // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
+        win->draw(spriteBackground);
+        win->draw(*spriteCharDisp);
 
         // Update the window
-        window.display();
+        win->display();
     }
 
+    shutdown();
+    
     return EXIT_SUCCESS;
 }
